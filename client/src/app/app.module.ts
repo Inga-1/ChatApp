9d7c30d897e36377ba88e2cd232f957ca7be5ca6
Inga Grigoryan <Inga.Grigoryan@Honeywell.com>
2023-06-01 14:02:10 +0200

    commit changes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +20,7 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule, 
     HttpClientModule, 
     FormsModule, 
-    BsDropdownModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
